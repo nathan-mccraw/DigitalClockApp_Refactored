42813194650d2202c7f0db6405e56e6bd0ee0f6d
nathan-mccraw <naterltw175@gmail.com>
2021-06-08 22:45:47 -0400

    Finished Product - except need to update link on ReadME

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DigitalClockApp_Refactored
+
+### A simple clock with date/time group.
+
+### Look at the project [here](https://nathan-mccraw.github.io/DigitalClockApp/).
+
 Refactored my digital clock app with the input from Trevor Mearns, a coach at the Software Development Mastermind Program
+
+![Preview Image](https://github.com/nathan-mccraw/DigitalClockApp/blob/master/Digital%20Clock%20Preview%20Image.png)
+
+### Summary:
+
+This is a refactored verison of my first JavaScript project. My first verison, I incorporated any knowledge I had in C++, referenced JavaScript MDN documents, and completed this project in my first attempt at writing JavaScript syntax. This second verison incorporated input from my coach, Trevor Mearns, from the Software Development Mastermind Program. This refacor includes the use of multiple functions, ternary operators, and a more streamlined writeup.
+
+#### Author:
+
+Nathan McCraw -- Inspiring Full Stack Developer -- [LinkedIn](https://www.linkedin.com/in/nathan-mccraw-5291535b/) [Personal Website - In Production]
--- script.js
+++ script.js
@@ -1,1 +1,82 @@
+function showTime() {
+  const dateTime = new Date();
+  const hours = dateTime.getHours();
+  const minutes = dateTime.getMinutes();
+  const seconds = dateTime.getSeconds();
+  const isAm = hours < 12 || hours === 0;
+  let amPm = isAm ? "AM" : "PM";
 
+  document.getElementById("time").textContent = `${formatHour(
+    hours
+  )}:${minutes}:${addLeadingZero(seconds)} ${amPm}`;
+}
+
+function addLeadingZero(number) {
+  return number < 10 ? "0" + number : number;
+}
+
+function formatHour(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
+
+  hour = hour === 0 ? hour + 12 : hour;
+
+  return hour;
+}
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function showDate() {
+  const dateTime = new Date();
+  const day = days[dateTime.getDay()];
+  const month = months[dateTime.getMonth()];
+  const date = dateTime.getDate();
+  const year = dateTime.getFullYear();
+
+  document.getElementById(
+    "date"
+  ).textContent = `${day} , ${month} ${formatSuffix(date)} ${year}`;
+}
+
+function formatSuffix(date) {
+  if (date < 10 || date > 20) {
+    switch (date % 10) {
+      case 1:
+        return `${date}st`;
+      case 2:
+        return `${date}nd`;
+      case 3:
+        return `${date}rd`;
+    }
+  }
+  return `${date}th`;
+}
+
+showDate();
+showTime();
+setInterval(() => {
+  showTime();
+  showDate();
+}, 1000);
